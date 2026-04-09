1fde82e40b741659206f326d4b2ef346df18dd99
Jürgen Mattheis <matthejue@gmail.com>
2026-04-09 16:06:46 +0200

    added string library, updated stdio libarary

---
+++ lib/string/string.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void *memcpy(void *destination, void *source, int count);
+void *memset(void *buffer, int value, int count);
+char *strcpy(char *destination, char *source);
+int strlen(char *string);
